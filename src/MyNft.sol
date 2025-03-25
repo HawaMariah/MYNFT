060ea7e71b40f352c37490aa4f39e62e6ae1eafa
Hawa Maria <hawamaria33@gmail.com>
2025-03-25 16:30:57 +0300

    updates

--- src/MyNft.sol
+++ src/MyNft.sol
@@ -1,2 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract MyNft is ERC721 {
+uint256 public currentTokenId;
+//keep track of the last minted tokens id
+
+// creates an ERC721 token called "MyNft" and symbol "MNFT"
+constructor () ERC721 ("MyNFT" , "MNFT"){}
+
+function mint (address recipient) public payable returns(uint256){
+uint256 newItemId = ++currentTokenId;
+_safeMint(recipient, newItemId);
+return newItemId;
+}
+}
--- test/MyNftTest.t.sol
+++ test/MyNftTest.t.sol
@@ -2,7 +2,34 @@
 pragma solidity ^0.8.20;
 
 import {Test} from "forge-std/Test.sol";
+import {MyNft} from "src/MyNft.sol";
 
 contract MyNftTest is Test{
-    
+     string constant NFT_NAME = "MyNFT";
+    string constant NFT_SYMBOL = "MNFT";
+MyNft public myNft;
+
+address alice = makeAddr("alice");
+address bob = makeAddr("bob");
+
+function setUp() public {
+    myNft = new MyNft();
+    }
+
+// test if mint function successfully passes
+function testMintPass() public {
+    myNft.mint(alice);
+     myNft.mint(bob);
+
+}
+
+
+// verifies the nft contract was initialized with expecte name and symbol
+      function testInitializedCorrectly() public view {
+        assert(keccak256(abi.encodePacked(myNft.name())) == keccak256(abi.encodePacked((NFT_NAME))));
+        assert(keccak256(abi.encodePacked(myNft.symbol())) == keccak256(abi.encodePacked((NFT_SYMBOL))));
+    }
+
+
+
 }
